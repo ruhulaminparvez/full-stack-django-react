d39403e111b2d359979988d5f797858b59989ec8
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-03-24 12:05:29 +0600

    changes added

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import Index
+from api.views import article_list, article_details
 
 urlpatterns = [
-    path('', Index, name='index'),
+    path('article', article_list, name='article_list'),
+    path('article/<int:pk>', article_details, name='article_list'),
 ]
