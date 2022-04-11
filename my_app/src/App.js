4839bdbc9d3b9c6760458a712bdd44b3bbf6f1c2
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-04-11 14:05:34 +0600

    Working with React Fragment :octocat:

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,10 +3,11 @@ import React from 'react';
 // import Hello from './components/Hello';
 // import FunComponent from './components/FunComponent';
 // import MyClass from './components/MyClass';
-import Name from './components/Name';
-import Example from './components/Example';
-import Example2 from './components/Example2';
-import Form from './components/Form';
+// import Name from './components/Name';
+// import Example from './components/Example';
+// import Example2 from './components/Example2';
+// import Form from './components/Form';
+import MyFragment from './components/MyFragment';
 
 
 function App() {
@@ -17,11 +18,13 @@ function App() {
 
   return (
     <div className='container'>  
-        <Name />
+        {/* <Name />
         <Example names = {['Python', 'Java', 'JavaScripts', 'C#']}/> 
         <Example2 names = {['React', 'React-Native', 'Django']}/>
 
-        <Form />
+        <Form /> */}
+
+        <MyFragment />
        
     </div>
   );
---
+++ my_app/src/components/MyFragment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyFragment() {
+  return (
+    <React.Fragment>
+        <h1>My Fragment Example</h1>
+
+        <p>This is Fragment</p>
+    </React.Fragment>
+  )
+}
+
+export default MyFragment
